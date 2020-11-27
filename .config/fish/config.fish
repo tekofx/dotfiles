b64448c56c9b88166d047e8bcccc73f2d9d7ebaa
Teko <tekofxx@gmail.com>
2020-11-27 23:27:32 +0100

    fish: fixed dotfiles alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ alias tree "exa -T"
 alias gs "git status"
 alias gp "git push"
 alias gc "git commit"
-alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias dotfiles "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 
 # fzf setup
 export FZF_DEFAULT_OPTS="
