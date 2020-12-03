7e6d18a53e1e19b336df30b99b566337a03859ad
Teko <tekofxx@gmail.com>
2020-12-03 17:41:58 +0100

    fish: Added git bare remove alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ alias dot "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 alias ds "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME status"
 alias da "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add" 
 alias dc "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME commit -m" 
+alias dr "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME remove" 
 alias dp "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME push" 
 
 ##########################################
