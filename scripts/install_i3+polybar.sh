716c3be019c6ebb01181452fee22bdde398fd4a0
Tekofx <tekofxx@gmail.com>
2022-01-28 17:09:45 +0100

    Added rofi

--- scripts/install_i3+polybar.sh
+++ scripts/install_i3+polybar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh i3-gaps
+yay -S polybar nerd-fonts-hack picom feh i3-gaps rofi
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/i3 $HOME/.config/ -r 
+cp ../.config/rofi $HOME/.config/ -r
