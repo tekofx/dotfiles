1b7abc4d94d506fd70f057917dc9a350cca1d430
Teko <tekofxx@gmail.com>
2022-01-28 17:05:41 +0100

    Fixed error

--- scripts/install_i3+polybar.sh
+++ scripts/install_i3+polybar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yay -S polybar nerd-font-hack picom feh i3-gaps
+yay -S polybar nerd-fonts-hack picom feh i3-gaps
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/i3 $HOME/.config/ -r 
