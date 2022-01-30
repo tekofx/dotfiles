c25506a922905a651f7a25881156565e67152f83
Teko <tekofxx@gmail.com>
2022-01-30 13:46:09 +0100

    Added bspwm, polybar and sxhkd configs

--- scripts/install_i3+polybar.sh
+++ scripts/install_i3+polybar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh i3-gaps rofi lxappearance
+yay -S polybar nerd-fonts-hack picom feh i3-gaps rofi lxappearance xdotool
+pip3 install i3-layouts
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/i3 $HOME/.config/ -r 
 cp ../.config/rofi $HOME/.config/ -r
