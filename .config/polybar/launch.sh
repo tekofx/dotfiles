30b790f999bbf7074054dd9488620826aa5573cb
Teko <tekofxx@gmail.com>
2022-01-31 16:30:48 +0100

    New configs for polybar+bspwm+sxhdk

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 killall polybar
-polybar main &
+polybar menu -c ~/.config/polybar/menu.ini &
+polybar workspaces -c ~/.config/polybar/workspaces.ini &
+polybar battery -c ~/.config/polybar/battery.ini &
+polybar date -c ~/.config/polybar/date.ini &
+polybar sound -c ~/.config/polybar/sound.ini &
+
+
+
