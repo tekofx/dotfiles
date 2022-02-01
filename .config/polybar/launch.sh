5e807a77023ad88df9c9b9bee82790455540fd22
Teko <tekofxx@gmail.com>
2022-02-01 12:20:24 +0100

    Changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,6 @@ polybar workspaces -c ~/.config/polybar/workspaces.ini &
 polybar battery -c ~/.config/polybar/battery.ini &
 polybar date -c ~/.config/polybar/date.ini &
 polybar sound -c ~/.config/polybar/sound.ini &
-
-
-
+polybar memory -c ~/.config/polybar/memory.ini &
+polybar storage -c ~/.config/polybar/storage.ini &
+polybar tray -c ~/.config/polybar/tray.ini &
