bb25a84d6e09efb71dd6a2e72a8d948ffad9e14b
Teko <tekofxx@gmail.com>
2022-02-01 12:56:09 +0100

    Added connection settings

--- scripts/install_bspwm+polybar.sh
+++ scripts/install_bspwm+polybar.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool
+yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/bspwm $HOME/.config/ -r 
 cp ../.config/sxhkd $HOME/.config/ -r 
 cp ../.config/rofi $HOME/.config/ -r
+
+# Config connection
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable wpa_supplicant.service
+sudo systemctl start NetworkManager.service
