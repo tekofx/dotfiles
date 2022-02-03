1cafd818ce125576661094ddc74548eb696e41dd
Teko <tekofxx@gmail.com>
2022-02-03 20:06:12 +0100

    Minor changes

--- .config/sxhkd/take_section_screenshot.sh
+++ .config/sxhkd/take_section_screenshot.sh
@@ -1,8 +1,6 @@
-mkdir -p ~/screenshots
 scrot -s screenshot_%Y%m%d_%H%M%S.png
-mv *png ~/screenshots/
-new_image=~/screenshots/$(exa ~/screenshots/ -1 -sold | head -1)
+mv *png ~/Pictures/
+new_image=~/Pictures/$(exa ~/Pictures/ -1 -sold | head -1)
 xclip -selection clipboard -t image/png -i $new_image
 
-echo $new_image
-dunstify -i $new_image  "Section Screenshot Taken" -t 2000
+dunstify -i $new_image  "Section Screenshot Taken" -t 2000 
