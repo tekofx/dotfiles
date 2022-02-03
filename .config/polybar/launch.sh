2d30821f87d994c4a32ebf22b7d646b7bc059adb
Teko <tekofxx@gmail.com>
2022-02-03 11:14:51 +0100

    Added dunst and changes to polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
+
+# Launch dunst
+killall dunst
+dunst &
+
+# Launch polybar
 killall polybar
-polybar menu -c ~/.config/polybar/menu.ini &
-polybar workspaces -c ~/.config/polybar/workspaces.ini &
-polybar battery -c ~/.config/polybar/battery.ini &
-polybar date -c ~/.config/polybar/date.ini &
-polybar sound -c ~/.config/polybar/sound.ini &
-polybar memory -c ~/.config/polybar/memory.ini &
-polybar storage -c ~/.config/polybar/storage.ini &
-polybar tray -c ~/.config/polybar/tray.ini &
+polybar top &
+polybar left &
+
+# Notify
+notify-send "Launched polybar" -t 3000
--- scripts/install_bspwm+polybar.sh
+++ scripts/install_bspwm+polybar.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet
+yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst dunstify
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/bspwm $HOME/.config/ -r 
 cp ../.config/sxhkd $HOME/.config/ -r 
 cp ../.config/rofi $HOME/.config/ -r
+cp ../.config/dunst $HOME/.config/ -r
+
 
 # Config connection
 sudo systemctl enable NetworkManager.service
