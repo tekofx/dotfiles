6f3c6c5f980c1defadd38c7100c033941f36198f
Tekofx <tekofxx@gmail.com>
2022-02-08 14:05:29 +0100

    Fix

--- scripts/install_bspwm+polybar.sh
+++ scripts/install_bspwm+polybar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst exa scrot xclip eww nerd-fonts-jetbrains-mono-nerd-font-symbols-complete-git 
-cp ../.config/polybar $HOME/.config/ -r
-cp ../.config/bspwm $HOME/.config/ -r 
-cp ../.config/sxhkd $HOME/.config/ -r 
-cp ../.config/rofi $HOME/.config/ -r
-cp ../.config/dunst $HOME/.config/ -r
-cp ../.config/eww $HOME/.config/ -r
+cp ../polybar $HOME/.config/ -r
+cp ../bspwm $HOME/.config/ -r 
+cp ../sxhkd $HOME/.config/ -r 
+cp ../rofi $HOME/.config/ -r
+cp ../dunst $HOME/.config/ -r
+cp ../eww $HOME/.config/ -r
 
 
 # Config connection
