7d1d7c5ec8ce2e4f2f79d75a54a292f14e551d4a
Tekofx <tekofxx@gmail.com>
2022-02-08 14:07:09 +0100

    Removed .config

--- .config/dunst/notify_volume_change.sh
+++
@@ -1,3 +0,0 @@
-var=$(amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
-
-dunstify  "Volume: " -h int:value:$var -u low -t 500 
--- .config/sxhkd/take_section_screenshot.sh
+++
@@ -1,6 +0,0 @@
-scrot -s screenshot_%Y%m%d_%H%M%S.png
-mv *png ~/Pictures/
-new_image=~/Pictures/$(exa ~/Pictures/ -1 -sold | head -1)
-xclip -selection clipboard -t image/png -i $new_image
-
-dunstify -i $new_image  "Section Screenshot Taken" -t 2000 
