1df82fd86a7d703e4baa4f2bb15c5ca141b90579
Tekofx <tekofxx@gmail.com>
2022-02-08 14:04:05 +0100

    Added eww and jetbrains

--- scripts/install_bspwm+polybar.sh
+++ scripts/install_bspwm+polybar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst exa scrot xclip mpc
+yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst exa scrot xclip eww nerd-fonts-jetbrains-mono-nerd-font-symbols-complete-git 
 cp ../.config/polybar $HOME/.config/ -r
 cp ../.config/bspwm $HOME/.config/ -r 
 cp ../.config/sxhkd $HOME/.config/ -r 
