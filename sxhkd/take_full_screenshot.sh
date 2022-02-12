db15cdfac1b73d47336e5ac77354c0d20ef435b9
Teko <tekofxx@gmail.com>
2022-02-12 12:46:40 +0100

    Edited screenshots

--- sxhkd/take_full_screenshot.sh
+++ sxhkd/take_full_screenshot.sh
@@ -1,6 +1,6 @@
-scrot screenshot_%Y%m%d_%H%M%S.png
-mv *png ~/Pictures/
-new_image=~/Pictures/$(exa ~/Pictures/ -1 -sold | head -1)
+mkdir $HOME/screenshots
+scrot $HOME/screenshots/screenshot_%Y%m%d_%H%M%S.png
+new_image=~/screenshots/$(exa ~/screenshots/ -1 -sold | head -1)
 xclip -selection clipboard -t image/png -i $new_image
 
 dunstify -i $new_image  "Full Screenshot Taken" -t 2000
