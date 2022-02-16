b323cc61cad4aa12b2131857f48a6d22c31fb80c
Tekofx <tekofxx@gmail.com>
2022-02-16 23:08:03 +0100

    Added script to configure a new arch-based installation

---
+++ scripts/arch_setup_minimal.sh
@@ -0,0 +1,21 @@
+# Setup a new arch based installation
+
+# Update packages
+sudo pacman -Syu
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
+rm $HOME/.fzf/ -dfr
+
+# Install needed packages
+sudo pacman -S yay --noconfirm
+yay -S exa lazydocker docker docker-compose fish navi duf ncdu neofetch neovim btop zip unzip --noconfirm
+
+# Add cheats to navi
+navi repo add https://github.com/Tekofx/navi-cheats.git
+
+# Configure fish
+cp ../fish/ $HOME/.config/ -r
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+chsh -s /usr/bin/fish
