d698f51b17a3b4d127b56a9430c3a097ec9a2896
tekofx <tekofxx@gmail.com>
2022-03-22 15:41:56 +0100

    Added brightness control

--- scripts/install_bspwm+polybar.sh
+++ scripts/install_bspwm+polybar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst exa scrot xclip eww nerd-fonts-jetbrains-mono
+yay -S polybar nerd-fonts-hack picom feh bspwm sxhkd rofi lxappearance xdotool wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dunst exa scrot xclip eww nerd-fonts-jetbrains-mono brightnessctl
 cp ../polybar $HOME/.config/ -r
 cp ../bspwm $HOME/.config/ -r 
 cp ../sxhkd $HOME/.config/ -r 
