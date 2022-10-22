2e5eb1b76898f540877548634e2a18d8c0aeb10b
tekofx <tekofxx@gmail.com>
2022-10-22 17:39:21 +0200

    Added desktop installation

--- scripts/arch_setup_minimal.sh
+++ scripts/arch_setup_minimal.sh
@@ -1,7 +1,7 @@
 # Setup a new arch based installation
 
 # Update packages
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
@@ -10,7 +10,7 @@ rm $HOME/.fzf/ -dfr
 
 # Install needed packages
 sudo pacman -S yay --noconfirm
-yay -S exa lazydocker docker docker-compose fish navi duf ncdu neofetch neovim btop zip unzip htop --noconfirm
+yay -S exa lazydocker docker docker-compose fish navi duf ncdu neofetch neovim btop zip unzip htop tere dua micro gitui --noconfirm
 
 # Add cheats to navi
 navi repo add https://github.com/Tekofx/navi-cheats.git
@@ -19,3 +19,17 @@ navi repo add https://github.com/Tekofx/navi-cheats.git
 cp ../fish/ $HOME/.config/ -r
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 chsh -s /usr/bin/fish
+
+
+# Install Desktop programs
+printf 'Is a desktop system (y/n)? '
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+    echo Installing desktop utilites
+    yay -S kitty --noconfirm
+    cp ../kitty $HOME/.config/ -r
+    
+else
+    echo No
+fi
