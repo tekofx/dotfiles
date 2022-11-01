373003226cd6390a1a5ec9907f1f497c3892e9a5
tekofx <tekofxx@gmail.com>
2022-11-01 17:23:10 +0100

    Added more buttons

--- drawing-tablet/setup_huion.sh
+++ drawing-tablet/setup_huion.sh
@@ -13,6 +13,7 @@ xsetwacom --set "$stylus_name" Button 2 "key +ctrl +alt +1 -1 -alt -ctrl"
 # Set buttons in tablet
 xsetwacom --set "$tablet_name" Button 1 "key b"
 xsetwacom --set "$tablet_name" Button 2 "key +ctrl +1 -1 -ctrl" 
+xsetwacom --set "$tablet_name" Button 9  "key +ctrl"
 xsetwacom --set "$tablet_name" Button 11  "key +space"
 xsetwacom --set "$tablet_name" Button 13 "key -"
 xsetwacom --set "$tablet_name" Button 14 "key +"
