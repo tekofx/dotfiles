63fc879cfdcd3023e6006a79adcc3a5ddc0177d5
tekofx <tekofxx@gmail.com>
2022-11-01 15:51:21 +0100

    Added more buttons

--- drawing-tablet/setup_huion.sh
+++ drawing-tablet/setup_huion.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 stylus_id=9
 screen=HDMI-0
-device_name='HID 256c:006d Pad pad'
+tablet_name='HID 256c:006d Pad pad'
+stylus_name='HID 256c:006d stylus'
 
 # Set tablet to main screen
 xinput map-to-output $stylus_id $screen
 
-# Set buttons
-xsetwacom --set "$device_name" Button 1 "key b"
+# Set button in stylus
+xsetwacom --set "$stylus_name" Button 2 "key +ctrl +alt +1 -1 -alt -ctrl" 
 
+# Set buttons in tablet
+xsetwacom --set "$tablet_name" Button 1 "key b"
+xsetwacom --set "$tablet_name" Button 2 "key +ctrl +1 -1 -ctrl" 
+xsetwacom --set "$tablet_name" Button 11  "key +space"
+xsetwacom --set "$tablet_name" Button 13 "key -"
+xsetwacom --set "$tablet_name" Button 14 "key +"
 
-xsetwacom --set "$device_name" Button 11 "key +ctrl +z -z -ctrl"
-xsetwacom --set "$device_name" Button 12 "key +ctrl +y -y -ctrl"
+xsetwacom --set "$tablet_name" Button 15 "key +ctrl +z -z -ctrl"
+xsetwacom --set "$tablet_name" Button 16 "key +ctrl +y -y -ctrl"
+
