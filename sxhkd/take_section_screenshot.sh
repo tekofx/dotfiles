30fac89ed4339926bdab894ede8c89077c10d3d6
tekofx <tekofxx@gmail.com>
2023-01-06 18:54:27 +0100

    Removed things

--- sxhkd/take_section_screenshot.sh
+++
@@ -1,6 +0,0 @@
-mkdir $HOME/screenshots
-scrot -s $HOME/screenshots/screenshot_%Y%m%d_%H%M%S.png
-new_image=~/screenshots/$(exa ~/screenshots/ -1 -sold | head -1)
-xclip -selection clipboard -t image/png -i $new_image
-
-dunstify -i $new_image  "Section Screenshot Taken" -t 2000 
